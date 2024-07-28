3bf2851e9b9644a82617cce8dd1324bb2118d4e2
Akash kumar Nayak <akashnayak3009@gmail.com>
2024-07-28 13:50:32 +0530

    pdf renderer page created

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,3 +1,4 @@
+import PdfRenderer from '@/components/PdfRenderer'
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound, redirect } from 'next/navigation'
@@ -21,14 +22,23 @@ const page = async ({ params }: PageProps) => {
     // make database call
     const file = await db.file.findFirst({
         where: {
-            id: fileid, 
+            id: fileid,
             userId: user.id
         }
     })
 
     if (!file) notFound()
     return (
-        <div>page{fileid}</div>
+        <div className='flex-1 justify-between flex flex-col h-[calc(100vh - 3.5rem)]'>
+            <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
+                {/* Left Side */}
+                <div className='flex-1 xl:flex'>
+                    <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6 '>
+                        <PdfRenderer/>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
