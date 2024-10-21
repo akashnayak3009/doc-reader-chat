ae9f96c336154c181572a2e4838a8d993a2e00de
Akash kumar Nayak <akashnayak3009@gmail.com>
2024-10-21 21:11:25 +0530

    added custom progress

--- src/app/trpc/index.ts
+++ src/app/trpc/index.ts
@@ -4,6 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from 'zod'
 
 
 export const appRouter = router({
@@ -43,7 +44,48 @@ export const appRouter = router({
       userId
     }
    })
-  })
+  }),
+
+
+  getFile: privateProcedure
+  .input(z.object({ key: z.string() }))
+  .mutation(async ({ ctx, input }) => {
+    const { userId } = ctx
+
+    const file = await db.file.findFirst({
+      where: {
+        key: input.key,
+        userId,
+      },
+    })
+
+    if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+    return file
+  }),
+
+  deleteFile: privateProcedure
+  .input(z.object({ id: z.string() }))
+  .mutation(async ({ ctx, input }) => {
+    const { userId } = ctx
+
+    const file = await db.file.findFirst({
+      where: {
+        id: input.id,
+        userId,
+      },
+    })
+
+    if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+    await db.file.delete({
+      where: {
+        id: input.id,
+      },
+    })
+
+    return file
+  }),
 });
 
 // Export type router type signature,
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,12 +4,16 @@ import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button';
 
 import Dropzone from 'react-dropzone'
-import { Cloud, File } from 'lucide-react';
+import { Cloud, File, Loader2 } from 'lucide-react';
 import { Progress } from './ui/progress';
 import { useUploadThing } from '@/lib/uploadthing';
 import { useToast } from '@/hooks/use-toast';
+import { trpc } from '@/app/_trpc/client';
+import { useRouter } from 'next/navigation';
 
 const UploadDropzone = () => {
+
+    const router = useRouter();
 
     const [isUploading, setIsUploading] = useState<boolean>(false);
     const [uploadProgress, setUploadProgress] = useState<number>(0);
@@ -17,6 +21,14 @@ const UploadDropzone = () => {
     const { toast } = useToast();
 
     const { startUpload } = useUploadThing("pdfUploader");
+
+    const {mutate: startPolling } = trpc.getFile.useMutation({
+        onSuccess: (file) => {
+            router.push(`/dashboard/${file.id}`)
+        },
+        retry: true,
+        retryDelay: 500
+    })
 
     const startSimulatedProgress = () => {
         setUploadProgress(0);
@@ -63,6 +75,8 @@ const UploadDropzone = () => {
 
         clearInterval(progressInterval);
         setUploadProgress(100)
+
+        startPolling({key})
     }}>
         {
             ({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -91,10 +105,24 @@ const UploadDropzone = () => {
                             {
                                 isUploading ? (
                                     <div className='w-full mt-4 max-w-xs mx-auto'>
-                                        <Progress value={uploadProgress} className='h-1 w-full bg-zinc-200' />
+                                        <Progress 
+                                        indicatorColor={
+                                            uploadProgress === 100 ? 'bg-green-500' : ''
+                                        }
+                                        value={uploadProgress} className='h-1 w-full bg-zinc-200' />
+                                      {
+                                        uploadProgress === 100 ? (
+                                            <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
+                                                <Loader2 className='h-3 w-3 animate-spin' />
+                                                Redirecting ....
+                                            </div>
+                                        ) : null
+                                      }
                                     </div>
+                                    
                                 ) : null
                             }
+                            <input {...getInputProps()} type='file' id='dropzone-file' className='hidden' />
                         </label>
                     </div>
                 </div>
